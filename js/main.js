1246ab748ed2962287a617577d31ab5f0d5fac6b
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2016-05-09 15:10:50 -0500

    correciones
    correciones en fix top menú

--- js/main.js
+++ js/main.js
@@ -59,3 +59,23 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 })
 
+
+jQuery(document).ready(function($) {
+  
+    // Fixa navbar ao ultrapassa-lo
+    var navbar = $('#menu'),
+        distance = navbar.offset().top,
+        $window = $(window);
+
+    $window.scroll(function() {
+        if ($window.scrollTop() >= distance) {
+            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+            $("body").css("padding-top", "0px");
+            $("section").css("padding-top", "60px");
+        } else {
+            navbar.removeClass('navbar-fixed-top');
+            $("body").css("padding-top", "0px");
+            $("section").css("padding-top", "0px");
+        }
+    });
+});
