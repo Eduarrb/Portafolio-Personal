e8f50d55e007d9d378e9c87db4266ef89309256e
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2016-08-12 11:24:58 -0500

    cambios scroll to
    mejoras para el scroll to. falta solucionar transisión del principal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 var $item = $('.carousel .item'); 
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
@@ -24,35 +26,7 @@ $('.carousel').carousel({
   pause: "false"
 });
 
-/*Scroll to*/
-jQuery(function( $ ){
-      /**
-       * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
-       * @see http://demos.flesler.com/jquery/scrollTo/
-       * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.localScroll.
-       */
-      
-      // The default axis is 'y', but in this demo, I want to scroll both
-      // You can modify any default like this
-      $.localScroll.defaults.axis = 'xy';
-      
-      /**
-       * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-       * also affect the >> and << links. I want every link in the page to scroll.
-       */
-      $.localScroll({
-        target: '#body', // could be a selector or a jQuery object too.
-        queue:true,
-        duration:1500,
-        hash:true,
-        onBefore:function( e, anchor, $target ){
-          // The 'this' is the settings object, can be modified
-        },
-        onAfter:function( anchor, settings ){
-          // The 'this' contains the scrolled element (#content)
-        }
-      });
-    });
+$('.navbar').css("top",$('.header').height())
 
 /*PopOver*/
 $(function () {
@@ -60,22 +34,269 @@ $(function () {
 })
 
 
-jQuery(document).ready(function($) {
-  
-    // Fixa navbar ao ultrapassa-lo
-    var navbar = $('#menu'),
-        distance = navbar.offset().top,
-        $window = $(window);
 
-    $window.scroll(function() {
-        if ($window.scrollTop() >= distance) {
-            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
-            $("body").css("padding-top", "0px");
-            $("section").css("padding-top", "60px");
-        } else {
-            navbar.removeClass('navbar-fixed-top');
-            $("body").css("padding-top", "0px");
-            $("section").css("padding-top", "0px");
+
+/*Scroll to*/
+
+// Sticky navigation with content section highlight.
+
+// Help from https://github.com/davist11/jQuery-One-Page-Nav and http://jqueryfordesigners.com/fixed-floating-elements/
+
+
+$(document).ready(function() {
+   
+  $('#main-nav-list').onePageNav({
+    scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
+    scrollOffset: 51 //Height of Navigation Bar
+  });
+  
+  // Sticky Header - http://jqueryfordesigners.com/fixed-floating-elements/         
+  var top = $('#menu').offset().top - parseFloat($('#menu').css('margin-top').replace(/auto/, 0));
+  
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+    
+    // whether that's below the form
+    if (y >= top) {
+      // if so, ad the fixed class
+      $('#menu').addClass('fixed');
+    } else {
+      // otherwise remove it
+      $('#menu').removeClass('fixed');
+    }
+  });
+  
+});
+
+// Custom 
+
+/*
+ * jQuery One Page Nav Plugin
+ * http://github.com/davist11/jQuery-One-Page-Nav
+ *
+ * Copyright (c) 2010 Trevor Davis (http://trevordavis.net)
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://jquery.org/license
+ *
+ * @version 2.2
+ *
+ * Example usage:
+ * $('#nav').onePageNav({
+ *   currentClass: 'current',
+ *   changeHash: false,
+ *   scrollSpeed: 750
+ * });
+ */
+
+;(function($, window, document, undefined){
+
+  // our plugin constructor
+  var OnePageNav = function(elem, options){
+    this.elem = elem;
+    this.$elem = $(elem);
+    this.options = options;
+    this.metadata = this.$elem.data('plugin-options');
+    this.$nav = this.$elem.find('a');
+    this.$win = $(window);
+    this.sections = {};
+    this.didScroll = false;
+    this.$doc = $(document);
+    this.docHeight = this.$doc.height();
+  };
+
+  // the plugin prototype
+  OnePageNav.prototype = {
+    defaults: {
+      currentClass: 'current',
+      changeHash: false,
+      easing: 'swing',
+      filter: '',
+      scrollSpeed: 750,
+      scrollOffset: 0,
+      scrollThreshold: 0.5,
+      begin: false,
+      end: false,
+      scrollChange: false
+    },
+
+    init: function() {
+      var self = this;
+      
+      // Introduce defaults that can be extended either
+      // globally or using an object literal.
+      self.config = $.extend({}, self.defaults, self.options, self.metadata);
+      
+      //Filter any links out of the nav
+      if(self.config.filter !== '') {
+        self.$nav = self.$nav.filter(self.config.filter);
+      }
+      
+      //Handle clicks on the nav
+      self.$nav.on('click.onePageNav', $.proxy(self.handleClick, self));
+
+      //Get the section positions
+      self.getPositions();
+      
+      //Handle scroll changes
+      self.bindInterval();
+      
+      //Update the positions on resize too
+      self.$win.on('resize.onePageNav', $.proxy(self.getPositions, self));
+
+      return this;
+    },
+    
+    adjustNav: function(self, $parent) {
+      self.$elem.find('.' + self.config.currentClass).removeClass(self.config.currentClass);
+      $parent.addClass(self.config.currentClass);
+    },
+    
+    bindInterval: function() {
+      var self = this;
+      var docHeight;
+      
+      self.$win.on('scroll.onePageNav', function() {
+        self.didScroll = true;
+      });
+      
+      self.t = setInterval(function() {
+        docHeight = self.$doc.height();
+        
+        //If it was scrolled
+        if(self.didScroll) {
+          self.didScroll = false;
+          self.scrollChange();
         }
+        
+        //If the document height changes
+        if(docHeight !== self.docHeight) {
+          self.docHeight = docHeight;
+          self.getPositions();
+        }
+      }, 250);
+    },
+    
+    getHash: function($link) {
+      return $link.attr('href').split('#')[1];
+    },
+    
+    getPositions: function() {
+      var self = this;
+      var linkHref;
+      var topPos;
+      var $target;
+      
+      self.$nav.each(function() {
+        linkHref = self.getHash($(this));
+        $target = $('#' + linkHref);
+
+        if($target.length) {
+          topPos = $target.offset().top;
+          self.sections[linkHref] = Math.round(topPos) - self.config.scrollOffset;
+        }
+      });
+    },
+    
+    getSection: function(windowPos) {
+      var returnValue = null;
+      var windowHeight = Math.round(this.$win.height() * this.config.scrollThreshold);
+
+      for(var section in this.sections) {
+        if((this.sections[section] - windowHeight) < windowPos) {
+          returnValue = section;
+        }
+      }
+      
+      return returnValue;
+    },
+    
+    handleClick: function(e) {
+      var self = this;
+      var $link = $(e.currentTarget);
+      var $parent = $link.parent();
+      var newLoc = '#' + self.getHash($link);
+      
+      if(!$parent.hasClass(self.config.currentClass)) {
+        //Start callback
+        if(self.config.begin) {
+          self.config.begin();
+        }
+        
+        //Change the highlighted nav item
+        self.adjustNav(self, $parent);
+        
+        //Removing the auto-adjust on scroll
+        self.unbindInterval();
+        
+        //Scroll to the correct position
+        $.scrollTo(newLoc, self.config.scrollSpeed, {
+          axis: 'y',
+          easing: self.config.easing,
+          offset: {
+            top: -self.config.scrollOffset
+          },
+          onAfter: function() {
+            //Do we need to change the hash?
+            if(self.config.changeHash) {
+              window.location.hash = newLoc;
+            }
+            
+            //Add the auto-adjust on scroll back in
+            self.bindInterval();
+            
+            //End callback
+            if(self.config.end) {
+              self.config.end();
+            }
+          }
+        });
+      }
+
+      e.preventDefault();
+    },
+    
+    scrollChange: function() {
+      var windowTop = this.$win.scrollTop();
+      var position = this.getSection(windowTop);
+      var $parent;
+      
+      //If the position is set
+      if(position !== null) {
+        $parent = this.$elem.find('a[href$="#' + position + '"]').parent();
+        
+        //If it's not already the current section
+        if(!$parent.hasClass(this.config.currentClass)) {
+          //Change the highlighted nav item
+          this.adjustNav(this, $parent);
+          
+          //If there is a scrollChange callback
+          if(this.config.scrollChange) {
+            this.config.scrollChange($parent);
+          }
+        }
+      }
+    },
+    
+    unbindInterval: function() {
+      clearInterval(this.t);
+      this.$win.unbind('scroll.onePageNav');
+    }
+  };
+
+  OnePageNav.defaults = OnePageNav.prototype.defaults;
+
+  $.fn.onePageNav = function(options) {
+    return this.each(function() {
+      new OnePageNav(this, options).init();
     });
+  };
+  
+})( jQuery, window , document );
+
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a')) {
+        $(this).collapse('hide');
+    }
 });
